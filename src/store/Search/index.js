429e45c17cb2341675cb10f4ae4e2427ad493156
tanshuai <1160390705@qq.com>
2022-07-06 17:21:47 +0800

    search组件完善

--- src/store/Search/index.js
+++ src/store/Search/index.js
@@ -22,7 +22,7 @@ const actions = {
 }
 const getters = {
   goodsList(state){
-    return state.searchList.goodsList
+    return state.searchList.goodsList || []
   },
   trademarkList(state){
     return state.searchList.trademarkList
