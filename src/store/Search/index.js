faa8bd62e5821b7660d8b70fb975b2547dcf7dab
softira <1160390705@qq.com>
2022-07-06 08:08:14 +0800

    search组件完善

--- src/store/Search/index.js
+++ src/store/Search/index.js
@@ -21,7 +21,15 @@ const actions = {
   }
 }
 const getters = {
-
+  goodsList(state){
+    return state.searchList.goodsList
+  },
+  trademarkList(state){
+    return state.searchList.trademarkList
+  },
+  attrsList(state){
+    return state.searchList.attrsList
+  },
 }
 
 export default {
